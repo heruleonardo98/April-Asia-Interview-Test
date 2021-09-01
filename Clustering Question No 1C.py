473e9a1ae27112781b23e068d1b47ca28283ce10
Heru Leonardo <65946494+heruleonardo98@users.noreply.github.com>
2021-09-01 20:54:37 +0700

    Add files via upload

---
+++ Clustering Question No 1C.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+# load the training dataset
+get_ipython().system('wget https://raw.githubusercontent.com/heruleonardo98/April-Asia-Interview-Test/main/ingredient.csv')
+data = pd.read_csv('ingredient.csv')
+
+# Display a random sample of 10 observations (just the features)
+features = data[data.columns[0:9]]
+features.sample(10)
+
+
+# As you can see, the dataset contains six data points (or *features*) for each instance (*observation*) of a seed. So you could interpret these as coordinates that describe each instance's location in six-dimensional space.
+# 
+# Now, of course six-dimensional space is difficult to visualise in a three-dimensional world, or on a two-dimensional plot; so we'll take advantage of a mathematical technique called *Principal Component Analysis* (PCA) to analyze the relationships between the features and summarize each observation as coordinates for two principal components - in other words, we'll translate the six-dimensional feature values into two-dimensional coordinates.
+
+# In[2]:
+
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+
+# Normalize the numeric features so they're on the same scale
+scaled_features = MinMaxScaler().fit_transform(features[data.columns[0:9]])
+
+# Get two principal components
+pca = PCA(n_components=2).fit(scaled_features)
+features_2d = pca.transform(scaled_features)
+features_2d[0:10]
+
+
+# Now that we have the data points translated to two dimensions, we can visualize them in a plot:
+
+# In[3]:
+
+
+import matplotlib.pyplot as plt
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+plt.scatter(features_2d[:,0],features_2d[:,1])
+plt.xlabel('Dimension 1')
+plt.ylabel('Dimension 2')
+plt.title('Data')
+plt.show()
+
+
+# Hopefully you can see at least two, arguably three, reasonably distinct groups of data points; but here lies one of the fundamental problems with clustering - without known class labels, how do you know how many clusters to separate your data into?
+# 
+# One way we can try to find out is to use a data sample to create a series of clustering models with an incrementing number of clusters, and measure how tightly the data points are grouped within each cluster. A metric often used to measure this tightness is the *within cluster sum of squares* (WCSS), with lower values meaning that the data points are closer. You can then plot the WCSS for each model.
+
+# In[5]:
+
+
+#importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Create 10 models with 1 to 10 clusters
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i)
+    # Fit the data points
+    kmeans.fit(features.values)
+    # Get the WCSS (inertia) value
+    wcss.append(kmeans.inertia_)
+    
+#Plot the WCSS values onto a line graph
+plt.plot(range(1, 11), wcss)
+plt.title('WCSS by Clusters')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+
+# ## Summary
+# The plot shows a large reduction in WCSS (so greater *tightness*) as the number of clusters increases from one to two, and a further noticable reduction from two to three clusters. After that, the reduction is less pronounced, resulting in an "elbow" in the chart at around three clusters. This is a good indication that there are two to three reasonably well separated clusters of data points.
+# 
+# 
